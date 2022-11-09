344bf4c4a0b884b65ff9dd8e0f869b20381ac435
Aleks_bass <105873742+izmailowAlex@users.noreply.github.com>
2022-11-09 23:48:14 +0300

    fix: fix mistake about 'exit'

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -1,14 +1,13 @@
-const process = require('process')
 const fs = require('fs')
 const path = require('path')
+const process = require('process')
 const { stdout, stdin } = require('process')
 
 const textFile = fs.createWriteStream(path.join(__dirname, 'text.txt'))
 
 stdout.write('Enter the text please...\n')
-
 stdin.on('data', (data) => {
-    if (data.toString() === 'exit') {
+    if (data.toString().trim() === 'exit') {
         process.exit();
     }
     textFile.write(data, (error) => {
@@ -17,8 +16,9 @@ stdin.on('data', (data) => {
         }
     })
 })
-
 process.on('SIGINT', () => process.exit());
-process.on('exit', () => {
-    stdout.write('Goodbye');
+process.on('exit', (code) => {
+    if (code === 0) {
+        stdout.write('Goodbye');
+    }
 });
